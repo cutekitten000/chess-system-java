2d368cc01147f13a44c8259f2474726e56e1f455
cutekitten000 <danilo.aguiar.dev@gmail.com>
2024-06-21 16:57:51 +0000

    Moving Pieces

--- chess-system/src/chess/ChessException.java
+++ chess-system/src/chess/ChessException.java
@@ -1,11 +1,9 @@
 package chess;
 
-import java.io.Serial;
 
-public class ChessException extends RuntimeException {
-    @Serial
-    private static final long serialVersionUID = 1L;
+import boardgame.BoardException;
 
+public class ChessException extends BoardException {
     public ChessException(String msg) {
         super(msg);
     }
