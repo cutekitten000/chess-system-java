5526e0bb4412403c9937ac59e9af6fe537d6ca85
Danilo Aguiar <danilo.aguiar.dev@gmail.com>
2024-07-22 00:27:49 -0300

    Fix clearing screen

--- chess-system/src/application/UI.java
+++ chess-system/src/application/UI.java
@@ -28,7 +28,7 @@ public class UI {
   public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
   public static void clearScreen() {
-    System.out.print("\033[H\033[22");
+    System.out.print("\033[H\033[2J");
     System.out.flush();
   }
 
